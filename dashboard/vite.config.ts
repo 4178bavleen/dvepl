e9bfe2579aaa820127a0dc6549a746fff6208e0d
Yashdeep9073 <ydeep9073@gmail.com>
2026-07-21 15:24:51 +0530

    chore

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,13 +1,18 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    port: 5175,
+    host: true, // Optional: allows access from other devices on the network
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
