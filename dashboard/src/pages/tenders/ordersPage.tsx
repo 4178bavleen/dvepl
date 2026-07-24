58b2ca21ea20cb3849636d8d91c08a27b246e719
Gabrial-8467 <gabrialdeora004@gmail.com>
2026-07-24 18:44:36 +0530

    fix

--- dashboard/src/pages/tenders/ordersPage.tsx
+++ dashboard/src/pages/tenders/ordersPage.tsx
@@ -658,7 +658,7 @@ export function OrdersPage() {
         });
       }
       setErrors(errs);
-      toast.error("Please complete all required fields before submitting.");
+      toast.error("Please fix form validation errors");
       return;
     }
 
