2bcc969b8ea493db32d9bbcb37011dc836c3175a
Gabrial-8467 <gabrialdeora004@gmail.com>
2026-08-14 10:11:39 +0530

    chore: update default theme to light in erpStore

--- dashboard/src/store/erpStore.ts
+++ dashboard/src/store/erpStore.ts
@@ -116,7 +116,7 @@ export const useERPStore = create<ERPStore>((set) => ({
   currentUserId: '',
   currentUserName: '',
   currentWorkspace: 'Default Workspace',
-  theme: 'dark',
+  theme: 'light',
   language: 'English',
 
   setCompanyId: (id) => set({ currentCompanyId: id }),
